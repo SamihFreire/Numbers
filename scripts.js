9a3dafd9d7e5bb7df1e67ddf050b0f45b4fdb67a
samih freire <samihcisne@gmail.com>
2025-08-20 17:16:31 -0300

    feat: criado as vrificações de valores dos inputs e função de sorteio

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+const form = document.querySelector("form");
 const numbers = document.getElementById("numbers");
 const fromNumber = document.getElementById("from");
 const toNumber = document.getElementById("to");
@@ -23,12 +24,47 @@ function onlyNumbers(item) {
     item.value = item.value.replace(/\D/g, "");
 }
 
-function checkValue(value) {
+function checkValues(newParameters) {
 
-    if(value < 1){
-        alert("Quantidade de números deve ser maior que 0!");
-        return 1;
+    if(newParameters.numbers < 1 || newParameters.fromNumber < 1 || newParameters.toNumber < 1){
+        alert("Os valoresm devem ser maior que 0!");
+        return false;
     }else{
-        return value;   
+        return true;   
     }
+}
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+
+    const newParameters = {
+        id: new Date().getTime(),
+        numbers: numbers.value,
+        fromNumber: fromNumber.value,
+        toNumber: toNumber.value,
+    }
+
+    drawNumbers(newParameters);
+
+}
+
+function drawNumbers(newParameters) {
+    try {
+        if(checkValues(newParameters)) {
+            let luckyNumbers = [];
+
+            for (let i = 0; i < newParameters.numbers; i++) {
+                luckyNumbers.push(getRandom(newParameters.fromNumber, newParameters.toNumber));
+            }
+            
+        }
+    } catch (error) {
+        alert("Não foi possível realizar o sortéio.");
+    }
+}
+
+function getRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
